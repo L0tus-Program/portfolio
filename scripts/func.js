1db222a0ece057cc511bc6911d2597759c8730c0
Felipe Gomes <62225877+L0tus-Program@users.noreply.github.com>
2022-06-14 16:25:46 -0300

    Curriculo e certificados atualizados

--- scripts/func.js
+++ scripts/func.js
@@ -5,3 +5,29 @@ $('a').click(function() {
     }, 1000);
     return false;
 });
+
+
+/*
+
+$(function(){ 
+    $("button").click(function(){
+        $("h1").css("color", "red").removeClass("red")
+     //   $("#mensagem").text("APERTEI ESSA PORRA").css("color","blue").fadeOut(5000)
+     $("#mensagem")
+        .text("APERTEI ESSA PORRA")
+        //.css("color","blue")
+        .css({
+            color:"blue",
+            border:"1px solid red"  // variaveis com traço como background-color ficam como backgroundColor kernel case
+        })
+        .fadeOut(5000)
+        .addClass("green") // adiciona uma classe ao elemento, nao pode entrar em conflito com um css inserido no jquery   -> jquery aplica CSS INLINE, que tem prioridade
+        
+    })
+    $("h3").delay(1000)
+    $("h3").fadeIn("fast") 
+   
+})
+
+*/
+
